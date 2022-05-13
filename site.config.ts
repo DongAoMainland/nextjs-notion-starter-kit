1f72e36e12bf0b5e388b2facc13dbd12fea07375
DongAoTony <99664434+DongAoTony@users.noreply.github.com>
2022-05-13 22:56:50 +0800

    Update site.config.ts
    修改个人信息和notion页面关联

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9943c9d74657482aa1b414a47a18ed34',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '分身二的家',
+  domain: 'https://www.notion.so/Personal-Home-9943c9d74657482aa1b414a47a18ed34',
+  author: '分身二',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'xxx',
+  github: 'DongAoTony',
+  linkedin: 'yyy',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
